ad51e487136cde63327b748529454a194dd01ca0
Eveliina <pevetiainen.ep@hotmail.com>
2013-09-08 22:14:49 +0300

    Hajoilua tiedostoihin

--- HuffmanCoding/src/huffmancoding/Main.java
+++ HuffmanCoding/src/huffmancoding/Main.java
@@ -1,7 +1,9 @@
-
 package huffmancoding;
 
 import huffmancoding.koodaaja.HuffmanCoding;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
@@ -9,23 +11,46 @@ import java.util.Scanner;
 
 /**
  * Huffman koodauksella tuotettu tiedonpakkausohjelma.
+ *
  * @author Eveliina
  */
 public class Main {
 
-    
     /**
      * Pääohjelma, josta ohjelman suoritus käynnistetään.
+     *
      * @param args
      */
     public static void main(String[] args) {
-        
-        Scanner lukija = new Scanner(System.in);
-                
-        // Tekstikäyttöliittymä
-        
-        HuffmanCoding pakkaaja = new HuffmanCoding(lukija);
-        pakkaaja.kaynnista();
-        
+
+//        Scanner lukija = new Scanner(System.in);
+//                
+//        // Tekstikäyttöliittymä
+//        
+//        HuffmanCoding pakkaaja = new HuffmanCoding(lukija);
+//        pakkaaja.kaynnista();
+
+        File file = new File("testi.txt");
+
+        ByteArrayOutputStream byteAOutStream;
+
+        try {
+            System.out.println(file.length());
+            byteAOutStream = new ByteArrayOutputStream((int) file.length());
+            FileInputStream fileIStream = new FileInputStream(file);
+//            byte[] bufferi = new byte[(int) file.length()];
+//            for (int lueNro; (lueNro = fileIStream.read(bufferi)) != -1;) {
+//                byteAOutStream.write(bufferi, 0, lueNro);
+//                System.out.println("lue " + lueNro + " tavuja");
+//            }
+
+
+        } catch (Exception e) {
+            System.out.println("ERRORI!! " + e.getMessage());
+            return;
+        }
+
+        byte[] tavut = byteAOutStream.toByteArray();
+
     }
 }
--- HuffmanCoding/src/huffmancoding/koodaaja/HuffmanCoding.java
+++ HuffmanCoding/src/huffmancoding/koodaaja/HuffmanCoding.java
@@ -1,53 +1,54 @@
-
 package huffmancoding.koodaaja;
 
 import huffmancoding.logiikka.Pakkaaja;
 import huffmancoding.logiikka.Syotekasittelija;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.util.Scanner;
 
 /**
  * Luokka kuvaa Huffman koodauksen toteuttavaa luokkaa.
+ *
  * @author Eveliina
  */
 public class HuffmanCoding {
-    
+
     /**
      * Lukija, joka lukee käyttäjän syötteet.
      */
-    
     private Scanner lukija;
-    
     /**
      * Käyttäjän antamien syötteiden käsittelijä.
      */
-    
     private Syotekasittelija syotekasittelija;
-    
+
     /**
      * Konstruktorissa luodaan uusi Huffman koodaaja.
+     *
      * @param lukija Käyttäjän syötteiden lukija.
      */
-    
     public HuffmanCoding(Scanner lukija) {
         this.lukija = lukija;
         this.syotekasittelija = new Syotekasittelija(lukija);
     }
-    
+
     /**
      * Käynnistää pakkaajan tai purkajan toiminnan.
      */
-
     public void kaynnista() {
-        
+
         String toiminto = this.syotekasittelija.toiminnonValinta();
+
         
-        if(toiminto.equals("pakkaus")){
-            Pakkaaja pakkaaja = new Pakkaaja(this.syotekasittelija);
-            pakkaaja.kaynnista();
-        }else {
-            return;
-        }
-                
+
+
+//        if(toiminto.equals("pakkaus")){
+//            Pakkaaja pakkaaja = new Pakkaaja(this.syotekasittelija);
+//            pakkaaja.kaynnista();
+//        }else {
+//            return;
+//        }
+
     }
-    
 }
